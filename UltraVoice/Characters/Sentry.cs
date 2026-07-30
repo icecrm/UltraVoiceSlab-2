f09474a395d34815e5bbed898763dc867804c59d
ics cram <81833740+icecrm@users.noreply.github.com>
2026-07-30 12:50:36 -0500

    fixed 2 sentry lines being switched

--- UltraVoice/Characters/Sentry.cs
+++ UltraVoice/Characters/Sentry.cs
@@ -1,4 +1,196 @@
-﻿using HarmonyLib;
+using HarmonyLib;
+using System.Collections;
+using UnityEngine;
+using UltraVoice.Utilities;
+
+namespace UltraVoice.Characters
+{
+    public class SentryCharacter
+    {
+        // Voice line storage
+        public static AudioClip[] DigInClips;
+        public static AudioClip[] ShootClips;
+        public static AudioClip[] InterruptPainClips;
+        public static AudioClip[] InterruptClips;
+        public static AudioClip[] KickClips;
+        public static AudioClip[] DeathClips;
+
+        public static readonly string[] DigInSubs =
+        {
+            "Now, steady…",
+            "Locking target!",
+            "Just getting my bearings…",
+            "Who does this guy think he is?",
+            "There you are!",
+            "Pride comes before the fall!",
+        };
+
+        public static readonly string[] ShootSubs =
+        {
+            "Predictable!",
+            "[laughter]",
+            "My pleasure!"
+        };
+
+        public static readonly string[] KickSubs =
+        {
+            "Just because the swordfreak got 'em doesnt mean you can, pal.",
+            "Alright, keep your distance, pal.",
+            "No touching the merchandise, pal.",
+        };
+
+        public static readonly string[] InterruptSubs =
+        {
+            "Ahhh, not again!",
+            "Okay, THAT’S cheating!",
+            "WHA- ASSHOLE!",
+            "You gotta be kidding!",
+            "Damn it."
+        };
+
+
+        public static void LoadVoiceLines(BepInEx.Logging.ManualLogSource logger)
+        {
+            DigInClips = new[]
+            {
+                UltraVoicePlugin.LoadClip("Sentry.tur_DigIn1.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_DigIn2.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_DigIn3.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_DigIn4.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_DigIn5.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_DigIn6.wav")
+            };
+
+            InterruptPainClips = new[]
+            {
+                UltraVoicePlugin.LoadClip("Sentry.tur_Pain1.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Pain2.wav"),
+            };
+
+            InterruptClips = new[]
+            {
+                UltraVoicePlugin.LoadClip("Sentry.tur_Interrupt1.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Interrupt2.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Interrupt3.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Interrupt4.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Interrupt5.wav")
+            };
+
+            ShootClips = new[]
+            {
+                UltraVoicePlugin.LoadClip("Sentry.tur_Fire1.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Fire2.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Fire3.wav")
+            };
+
+            KickClips = new[]
+            {
+                UltraVoicePlugin.LoadClip("Sentry.tur_Kick1.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Kick2.wav"),
+                UltraVoicePlugin.LoadClip("Sentry.tur_Kick3.wav")
+            };
+
+//          DeathClips = new[]
+//          {
+//              UltraVoicePlugin.LoadClip("Sentry.tur_Death1.wav"),
+//              UltraVoicePlugin.LoadClip("Sentry.tur_Death2.wav"),
+//         };
+
+            logger.LogInfo("Sentry voice lines loaded successfully!");
+        }
+    }
+
+    [HarmonyPatch(typeof(Turret), "StartAiming")]
+    class SentryStartAimingPatch
+    {
+        static void Postfix(Turret __instance)
+        {
+            if (!UltraVoicePlugin.SentryVoiceEnabled.value) return;
+
+            VoiceManager.PlayRandomVoice(__instance, "Sentry",
+                SentryCharacter.DigInClips,
+                SentryCharacter.DigInSubs,
+                false
+            );
+        }
+    }
+
+    [HarmonyPatch(typeof(Turret), "Shoot")]
+    class SentryShootPatch
+    {
+        static void Postfix(Turret __instance)
+        {
+            if (!UltraVoicePlugin.SentryVoiceEnabled.value) return;
+
+            VoiceManager.PlayRandomVoice(__instance, "Sentry",
+                SentryCharacter.ShootClips,
+                SentryCharacter.ShootSubs,
+                false
+            );
+        }
+    }
+
+    [HarmonyPatch(typeof(Turret), "Kick")]
+    class SentryKickPatch
+    {
+        static void Postfix(Turret __instance)
+        {
+            if (!UltraVoicePlugin.SentryVoiceEnabled.value) return;
+
+            VoiceManager.PlayRandomVoice(__instance, "Sentry",
+                SentryCharacter.KickClips,
+                SentryCharacter.KickSubs,
+                false
+            );
+        }
+    }
+
+    [HarmonyPatch(typeof(Turret), "Interrupt")]
+    class SentryInterruptPatch
+    {
+        static void Postfix(Turret __instance)
+        {
+            if (!UltraVoicePlugin.SentryVoiceEnabled.value) return;
+
+            VoiceManager.PlayRandomVoice(__instance, "Sentry",
+                SentryCharacter.InterruptPainClips,
+                null,
+                true
+            );
+
+            UltraVoicePlugin.Instance.StartCoroutine(Interrupt(__instance));
+
+            static IEnumerator Interrupt(Turret turret)
+            {
+                yield return new WaitForSeconds(0.75f);
+
+                if (turret == null)
+                    yield break;
+
+                VoiceManager.PlayRandomVoice(turret, "Sentry",
+                    SentryCharacter.InterruptClips,
+                    SentryCharacter.InterruptSubs,
+                    true
+                );
+            }
+        }
+    }
+
+ // [HarmonyPatch(typeof(Turret), "OnDeath")]
+//  class SentryDeathPatch
+//  {
+//      static void Postfix(Turret __instance)
+//      {
+//          if (!UltraVoicePlugin.SentryVoiceEnabled.value) return;
+//
+//          VoiceManager.PlayRandomVoice(__instance, "Sentry",
+//              SentryCharacter.UseSentryClips(SentryCharacter.DeathClips, SentryCharacter.DeathClipsGoob),
+//              null,
+//              true
+//          );
+//      }
+//  }
+}﻿using HarmonyLib;
 using System.Collections;
 using UnityEngine;
 using UltraVoice.Utilities;
